dc2fef4004cf5208966b33d1170be11f2d0dcadb
guillercv123 <guillercv123@gmail.com>
2025-01-09 08:36:24 -0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    exportTrailingSlash: true,
+    output: 'export',
+    images: {
+        unoptimized: true, // Necesario para exportación estática
+    },
 };
 
 export default nextConfig;
