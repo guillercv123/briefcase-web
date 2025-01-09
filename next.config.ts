3c287f8637d043c06bb9f99887146f35ac022c43
guillercv123 <guillercv123@gmail.com>
2025-01-09 08:23:39 -0500

    update nextjs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     exportTrailingSlash: true,
+    exportPathMap: async function () {
+        return {
+            '/': { page: '/' },
+            // Aquí puedes agregar más rutas si las tienes en tu proyecto
+        };
+    },
 };
 
 export default nextConfig;
